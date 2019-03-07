b2e316793782371e5bec91cea53cd618ae96728a
beckermy <beckermy@grinnell.edu>
2019-03-07 16:27:34 -0600

    Revert the previous two commits when I did Djanog
    This reverts commit 5a80e33cc634e4e29a2a5e075b6abc9df0930ea2.

--- popcorn_analyzer/popcorn_analyzer/settings.py
+++ popcorn_analyzer/popcorn_analyzer/settings.py
@@ -15,12 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SETTINGS_DIR = os.path.dirname(__file__)
-PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
-PROJECT_ROOT = os.path.abspath(PROJECT_PATH)
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates'),
-)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -43,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'markdown_deux',
 )
 
 MIDDLEWARE_CLASSES = (
