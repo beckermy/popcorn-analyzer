128f54d95300ab47239f1fe9cce89dd0c6d4af05
Henry <henry.mahar@gmail.com>
2019-05-17 03:05:09 -0500

    Cleaned up comments, mel spectrogram

--- ThinkDSP-master/code/popcorn_analyzer_matlab.py
+++ ThinkDSP-master/code/popcorn_analyzer_matlab.py
@@ -8,62 +8,100 @@ import librosa
 from functools import reduce
 from scipy import signal as sig
 from scipy.interpolate import interp1d
+
+
+
+def create_filter(cutOff, sampling_rate, filter_order=6, type='lowpass'):
+	nyq = 0.5 * sampling_rate
+	N  = filter_order    # Filter order
+	fc = cutOff / nyq # Cutoff frequency normal
+	return sig.butter(N, fc, type)
+
+	
+def suggest_height(arr):
+	avg = sum(arr) / len(arr)
+	print(avg)
+
+	
 numpy.set_printoptions(threshold=numpy.inf)
 
-y, s = librosa.load('short_pops.wav', sr=22000) # Downsample 44.1kHz to 8kHz
 
 
-##################Create low-pass filter######################
-cutOff = 2000 # Cutoff frequency
-nyq = 0.5 * s
-N  = 6    # Filter order
-fc = cutOff / nyq # Cutoff frequency normal
-b, a = sig.butter(N, fc)
-#Apply filter
-temp_lp = sig.filtfilt(b, a, y)
+cur_sample_rate = 22000
+y, s = librosa.load('short_pops.wav', sr=cur_sample_rate) # Downsample 44.1kHz to 8kHz
 
 
+#Create and apply low pass filter
+low_pass_numerator, low_pass_denominator = create_filter(1000, s)
+low_pass_signal = sig.filtfilt(low_pass_numerator, low_pass_denominator, y)
 
-#high pass
-cutOff = 500 # Cutoff frequency
-nyq = 0.5 * s
-N  = 6    # Filter order
-fc = cutOff / nyq # Cutoff frequency normal
-b, a = sig.butter(N, fc, 'highpass')
-#Apply filter
-temp_hp = sig.filtfilt(b, a, y)
+#Create and apply high pass filter
+high_pass_numerator, high_pass_denominator = create_filter(1000, s, type='highpass')
+high_pass_signal = sig.filtfilt(high_pass_numerator, high_pass_denominator, y)
 
-######################Apply onset strength filter##################
-onset = librosa.onset.onset_strength(y, s,)
-
-#Onset of low pass filtered audio
-onset2 = librosa.onset.onset_strength(temp_hp, s,)
-
-
-#####################Mel frequency filter##########################
-mel_filter_params = 'n_mels=128, fmin=0.0, fmax=20000.0, htk=False, norm=1'
-
-mel_spectrogram = librosa.feature.melspectrogram(y, s, None, 4096, 512, .1)
-mel_spectrogram_test = librosa.feature.melspectrogram(y, s, None, 4096, 256, .1)
-onset_custom_mel = librosa.onset.onset_strength(temp_hp, s, mel_spectrogram)
-onset_custom_mel_test = librosa.onset.onset_strength(temp_hp, s, mel_spectrogram_test)
+#Generate and apply log mel spectrogram
+mel_spectrogram = librosa.feature.melspectrogram(y, s, None, 4096, 256, .1)
+onset_custom = librosa.onset.onset_strength(y, s, mel_spectrogram)
 
 
 #####################Interpolation attempt#############################
 
-################Display######################
+#num_samples = len(onset_custom_mel_test)
 
+#x = numpy.linspace(0, num_samples, num=num_samples, endpoint=True)
+#interpolation_test = interp1d(x, onset_custom_mel_test, kind='nearest')
+
+#xnew = numpy.arange(0, num_samples, .5)
+#ynew = interpolation_test(xnew)
+
+
+###################Savitzky–Golay filter###############################
+
+#SG_filter = sig.savgol_filter(onset_custom, 9, 4)
+
+
+########################Peak finding###################################
+#calculate samples in file
+num_samples = len(onset_custom)
+samples_in_file = num_samples * 256
+
+#suggest height
+suggest_height(onset_custom)
+
+#Find peaks in original signal
+peaks_original, properties_original = sig.find_peaks(y, height=.005)
+
+#Find peaks in onset strength signal
+peaks, properties = sig.find_peaks(onset_custom, height=.005)
+
+#Print original peaks
+print(len(peaks_original))
+times_original = map(lambda hop_num: round(hop_num / cur_sample_rate, 2), peaks_original)
+print(list(times_original))
+
+
+print(len(peaks))
+
+times = map(lambda hop_num: round(((hop_num * 256) / cur_sample_rate), 2), peaks)
+print(list(times))
+
+
+
+
+################Display######################
+plt.figure(1)
+plt.title('Original')
+plt.plot(y)
+plt.grid()
 
 plt.figure(2)
 plt.title('Onset')
-plt.plot(onset_custom_mel)
+plt.plot(onset_custom)
 plt.grid()
 
 plt.figure(3)
-plt.title('Onset of custom mel')
-plt.plot(onset_custom_mel_test)
+plt.title('test')
+plt.plot(mel_spectrogram)
 plt.grid()
-
-
 
 plt.show()
